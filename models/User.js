aef345809eb1e7af767652ec1fda2345afce739e
dymoy <148160802+dymoy@users.noreply.github.com>
2024-03-15 22:29:05 -0700

    Created User and Thought models. Created Reaction schema.

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+/* Define the schema to create User model */
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'Username is required!',
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'Email address is required!',
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        }, 
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought"
+
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+/* Create a virtual property 'friendCount' that retrieves the length of the user's friends array field on query */
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+/* Initialize the User model */
+const User = model('user', userSchema);
+module.exports = User;
